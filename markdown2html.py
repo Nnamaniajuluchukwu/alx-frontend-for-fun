4236f58db9501fb31e7480d9c2dbd981c192bdb0
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-08-16 17:02:39 +0000

    using sys to print to stderr

--- markdown2html.py
+++ markdown2html.py
@@ -5,13 +5,14 @@ import sys
 import os
 
 
-args = sys.argv
-if len(args) < 3:
-    sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
-    exit(1)
+if __name__ == "__main__":
+    args = sys.argv
+    if len(args) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
 
-if os.path.exists("./README.md"):
-    exit(0)
-else:
-    sys.stderr.write("Missing {}".format(args[1]))
-    exit(1)
+    if os.path.exists("./README.md"):
+        exit(0)
+    else:
+        sys.stderr.write("Missing {}".format(args[1]))
+        exit(1)
