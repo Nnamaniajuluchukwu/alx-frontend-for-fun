c9600b0cf88618f351db93f837a870e06b2e5cb0
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-08-16 16:50:15 +0000

    print to standard error

--- markdown2html.py
+++ markdown2html.py
@@ -7,11 +7,11 @@ import os
 
 args = sys.argv
 if len(args) < 3:
-    print("Usage: ./markdown2html.py README.md README.html")
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
     exit(1)
 
 if os.path.exists("./README.md"):
     exit(0)
 else:
-    print("Missing {}".format(args[1]))
+    print("Missing {}".format(args[1]), file=sys.stderr)
     exit(1)
