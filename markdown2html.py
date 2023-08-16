fa78e0c4cb5379ce74569e1566c812f00842ee04
Nnamani Joel Ajuluchukwu <daniel.eyang@yahoo.com>
2023-08-16 19:19:14 +0100

    convert markdown to html

--- markdown2html.py
+++ markdown2html.py
@@ -3,6 +3,7 @@
 """
 import sys
 import os
+import markdown
 
 
 if __name__ == "__main__":
@@ -11,8 +12,18 @@ if __name__ == "__main__":
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
 
-    if os.path.exists(args[1]):
+    try:
+        # Convert the Markdown to HTML
+        with open(args[1], 'r') as f:
+            html = markdown.markdown(f.read())
+
+        # Write the HTML to the output file
+        with open(args[2], 'w') as f:
+            f.write(html)
+
+        # Exit successfully
         sys.exit(0)
-    else:
-        sys.stderr.write("Missing {}".format(args[1]))
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Missing {args[1]}\n")
         sys.exit(1)
