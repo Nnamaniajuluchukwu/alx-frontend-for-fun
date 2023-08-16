81bb77ecae4a2c24caf2e11d04bb0ba365b81a73
Ajuluchukwu Nnamani Joel <daniel.eyang@yahoo.com>
2023-08-16 16:11:58 +0000

    markdown2html and readme file

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" validate a markdown file
+"""
+import sys
+import os
+
+
+args = sys.argv
+if len(args) < 3:
+    print("Usage: ./markdown2html.py README.md README.html")
+    exit(1)
+
+if os.path.exists("./README.md"):
+    exit(0)
+else:
+    print("Missing {}".format(args[1]))
+    exit(1)
